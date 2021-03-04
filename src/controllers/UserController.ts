73c87dce2316922d732058902f5568e78e766f70
Mike Alves <michelalvs@gmail.com>
2021-03-04 00:49:21 -0300

    Finished day 5. Updated Automated tests. Created custom Errors

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,28 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from 'yup';
+import { AppError } from "../errors/AppError";
 
 class UserController {
 
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required("Nome é um campo obrigatório!"),
+            email: yup.string().email().required("Email não informado ou em formato inválido.")
+        })
+
+        try {
+            await schema.validate(request.body, { abortEarly: false });
+
+        } catch (err) {
+            throw new AppError(err);
+        }
+
+
+
 
         const usersRepository = getCustomRepository(UsersRepository);
 
@@ -16,9 +33,7 @@ class UserController {
         });
 
         if (userAlreadyExists) {
-            return response.status(400).json({
-                error: "User with informed e-mail already exists!"
-            })
+            throw new AppError("User with informed e-mail already exists!")
         }
 
         const user = usersRepository.create({
@@ -30,6 +45,16 @@ class UserController {
 
 
         return response.status(201).json(user);
+
+    }
+
+
+    async show(request: Request, response: Response) {
+        const usersRepository = getCustomRepository(UsersRepository);
+
+        const showAll = await usersRepository.find();
+
+        return response.json(showAll);
 
     }
 
